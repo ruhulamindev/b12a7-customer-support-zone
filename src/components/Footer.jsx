1cadc51f62146371604d757ed899988a35d70c10
Ruhul Amin <ruhulamin824854@gmail.com>
2025-09-25 19:16:59 +0600

    navbar complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,55 +1,58 @@
 import React from "react";
+import Container from "./container";
 
 const Footer = () => {
   return (
     <footer className="bg-black text-gray-300 px-8 py-12">
-      <div className="grid md:grid-cols-5 gap-8 mb-8">
-        <div>
-          <h3 className="text-white">CS — Ticket System</h3>
-          <p className="text-sm mt-1">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
+      <Container>
+        <div className="grid md:grid-cols-5 gap-8 mb-8">
+          <div>
+            <h3 className="text-white">CS — Ticket System</h3>
+            <p className="text-sm mt-1">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+          <div>
+            <h4 className="text-white font-semibold mb-2">Company</h4>
+            <ul className="space-y-1 text-sm">
+              <li>About Us</li>
+              <li>Our Mission</li>
+              <li>Contact Saled</li>
+            </ul>
+          </div>
+          <div>
+            <h4 className="text-white font-semibold mb-2">Services</h4>
+            <ul className="space-y-1 text-sm">
+              <li>Products & Services</li>
+              <li>Customer Stories</li>
+              <li>Download Apps</li>
+            </ul>
+          </div>
+          <div>
+            <h4 className="text-white font-semibold mb-2">Information</h4>
+            <ul className="space-y-1 text-sm">
+              <li>Privacy Policy</li>
+              <li>Terms & Conditions</li>
+              <li>Join Us</li>
+            </ul>
+          </div>
+          <div>
+            <h4 className="text-white font-semibold mb-2">Social Links</h4>
+            <ul className="space-y-1 text-sm">
+              <li>@CS — Ticket System</li>
+              <li>@CS — Ticket System</li>
+              <li>@CS — Ticket System</li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <h4 className="text-white font-semibold mb-2">Company</h4>
-          <ul className="space-y-1 text-sm">
-            <li>About Us</li>
-            <li>Our Mission</li>
-            <li>Contact Saled</li>
-          </ul>
+        <hr />
+        <div className="text-center md:text-center text-gray-500 text-sm mt-8">
+          © 2025 CS — Ticket System. All rights reserved.
         </div>
-        <div>
-          <h4 className="text-white font-semibold mb-2">Services</h4>
-          <ul className="space-y-1 text-sm">
-            <li>Products & Services</li>
-            <li>Customer Stories</li>
-            <li>Download Apps</li>
-          </ul>
-        </div>
-        <div>
-          <h4 className="text-white font-semibold mb-2">Information</h4>
-          <ul className="space-y-1 text-sm">
-            <li>Privacy Policy</li>
-            <li>Terms & Conditions</li>
-            <li>Join Us</li>
-          </ul>
-        </div>
-        <div>
-          <h4 className="text-white font-semibold mb-2">Social Links</h4>
-          <ul className="space-y-1 text-sm">
-            <li>@CS — Ticket System</li>
-            <li>@CS — Ticket System</li>
-            <li>@CS — Ticket System</li>
-          </ul>
-        </div>
-      </div>
-      <hr/>
-      <div className="text-center md:text-center text-gray-500 text-sm mt-8">
-        © 2025 CS — Ticket System. All rights reserved.
-      </div>
+      </Container>
     </footer>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from "react";
+import Container from "./container";
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="bg-purple-100 shadow-md">
+      <Container>
+        <nav className="py-4 flex justify-between items-center">
+          <h1 className="md:text-xl text-sm font-bold">CS — Ticket System</h1>
+          <div className="md:flex hidden items-center gap-6">
+            <a className="text-gray-600" href="">
+              Home
+            </a>
+            <a className="text-gray-600" href="">
+              FAQ
+            </a>
+            <a className="text-gray-600" href="">
+              Changelog
+            </a>
+            <a className="text-gray-600" href="">
+              Blog
+            </a>
+            <a className="text-gray-600" href="">
+              Download
+            </a>
+            <a className="text-gray-600" href="">
+              Contact
+            </a>
+            <button className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white font-semibold text-sm px-4 py-2 rounded">
+              + New Ticket
+            </button>
+          </div>
+        </nav>
+      </Container>
+    </div>
+  );
 };
 
 export default Navbar;
